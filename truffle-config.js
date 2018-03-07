97238699ede3958234c0e0c7df2ffc0d1c545697
BlockchainBud <blockchainbud@gmail.com>
2018-03-07 15:14:57 -0500

    Update truffle-config and gitignore

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,14 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*",
+      gas: 4500000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+        runs: 200
     }
   }
 };
