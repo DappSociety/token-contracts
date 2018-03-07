e31a8b509dbfb74ba04a95b9250208dfb42e2c7d
BlockchainBud <blockchainbud@gmail.com>
2018-03-07 10:03:37 -0500

    Initialize Truffle

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+// Use this file to easily deploy the contracts you're writing.
+
+// var Contract = artifacts.require('Contract');
+module.exports = function (deployer) {
+  // deployer.deploy(Contract);
+};
